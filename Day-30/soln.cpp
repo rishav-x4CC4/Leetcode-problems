8e0164a241a86f3b4b5672faa9241b0b6264b959
rishav-x4CC4 <rishav870935@gmail.com>
2026-07-15 10:19:18 +0530

    asdad

--- Day-30/soln.cpp
+++ Day-30/soln.cpp
@@ -13,3 +13,21 @@ int main() {
 
     return 0;
 }
+
+
+// class Solution {
+// public:
+//     int gcdOfOddEvenSums(int n) {
+//         int sumOdd =  n*n;
+//         int sumEven = n*(n+1);
+//         if(sumOdd==0 || sumEven==0) return max(sumEven, sumOdd);
+//         int result = min(sumOdd, sumEven);
+//         while (result > 0) {
+//             if (sumOdd % result == 0 && sumEven % result == 0) {
+//             break;
+//             }
+//         result--;
+//         }
+//         return result;
+//     }
+//     }; brute force approach
